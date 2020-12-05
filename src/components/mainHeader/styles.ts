99118ee9bb68f68b2b59ccd73c545b3e9c580564
Joao Martins <joaopaulomartinspro@gmail.com>
2020-12-05 13:51:29 -0100

    created the profile with emojis on header

--- src/components/mainHeader/styles.ts
+++ src/components/mainHeader/styles.ts
@@ -2,8 +2,20 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
 grid-area: MH;
+display: flex;
+justify-content: space-between;
 background-color: ${(props) => props.theme.colors.secondary};
-
+align-items:center;
+padding: 0 10px;
+border-bottom: 1px solid ${props => props.theme.colors.gray};
 `;
+
+export const Profile = styled.div`
+    color: ${props => props.theme.colors.white};
+`;
+export const Welcome = styled.h3``;
+export const UserName = styled.span``;
+
+
 
 
