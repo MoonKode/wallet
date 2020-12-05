8e5f891aa64c6d4772aba4841dc08c0e85caf098
Joao Martins <joaopaulomartinspro@gmail.com>
2020-12-05 14:28:52 -0100

    Added eslint and prettier

---
+++ src/components/toggle/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container, ToggleLabel } from './styles'
+
+const Toggle: React.FC = () => (
+    <Container>
+      
+        <ToggleLabel>Light</ToggleLabel>
+        <ToggleLabel>Dark</ToggleLabel>
+    </Container>
+)
